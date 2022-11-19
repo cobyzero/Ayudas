bc542839e9ab52d80558a0437f2af59e241ac075
Sebastian Abal Salazar <115430199+cobyzero@users.noreply.github.com>
2022-11-19 11:24:27 -0500

    a

--- Ayuda1.cpp
+++ Ayuda1.cpp
@@ -1,14 +1,36 @@
 #include <iostream>
 using namespace std;
 
+
+bool esMultiplode2(int i) {
+
+	if (i % 2 == 0)
+	{
+		return true;
+	}
+	return false;
+}
+
+bool esMultiplode3(int i) {
+
+	if (i % 3 == 0)
+	{
+		return true;
+	}
+
+	return false;
+}
+
+
+
 int main() {
 
 
 	for (size_t i = 1; i < 1000; i++)
 	{
-		if (i % 2 == 0)
+		if (esMultiplode2(i))
 		{
-			if (!i % 3 == 0)
+			if (!esMultiplode3(i))
 			{
 				cout << i << endl;
 			} 
